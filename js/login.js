2ae7ce2db3d0d7fcfc8191b2a874c01d228d5161
adisapre <adi.sapre@gmail.com>
2020-07-21 16:58:03 -0400

    updated login and css for login again

--- js/login.js
+++ js/login.js
@@ -25,36 +25,4 @@ function activateLogin() {
     document.getElementById("login-form").style.display = "block"
 
 }
-function validateUser(desc) {
-    var usererror = document.getElementById("username");
-    if(desc.value === "") {
-        usererror.innerHTML = "<span class=\"error\" id=\"username-note\" style='color: red;'>" +
-            "Please enter a username</span>"
-        document.getElementById("username").focus();
-        return false;
-    }
-    else {
-        return true;
-    }}
 
-
-
-/*$(function() {
-
-    $('#login-form-link').click(function(x) {
-        $("#login-form").delay(100).fadeIn(100);
-        $("#register-form").fadeOut(100);
-        $('#register-form-link').removeClass('active');
-        $(this).addClass('active');
-        x.preventDefault();
-    });
-    $('#register-form-link').click(function(e) {
-        $("#register-form").fadeIn(100);
-        $("#login-form").fadeOut(100);
-        $('#login-form-link').removeClass('active');
-        $(this).addClass('active');
-        x.preventDefault();
-    });
-
-});
-*/
