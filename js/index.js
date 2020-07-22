7f0320138910697dec72344ee643408b69a1b107
Thomas Davis <tad8tt@virginia.edu>
2020-07-21 23:14:01 -0400

    Fixed some stuff

--- js/index.js
+++ js/index.js
@@ -1,36 +1,38 @@
-var rownumber = 0;
-
+var rowNumber = 1; //keep track of number of rows for rendering
 function addRow()
 {
-    if (rownumber = 2) {
-        var name= "Discover IT Student";
-        var rewards= ["5% Cashback Restaurants", "2% Cashback Grocery Stores", "1% Cashback Everywhere"];
-        var benefits= rownumber++;
-        var balance= 407;
-        var limit= 1000;
-    } else if (rownumber = 1) {
-        var name= "Amazon Prime Rewards Card";
-        var rewards= ["5% Cashback Amazon", "3% Cashback Gas Stations", "4% Cashback Home Improvement", "1% Cashback Everywhere"];
-        var benefits= "Interest-free Amazon payments";
-        var balance= 3970;
-        var limit= 4000;
-    } else {
-        var name= "Dummy Card";
-        var rewards= ["5% Cashback", "3% Cashback", "4% Cashback", "1% Cashback"]
-        var benefits= "Useless";
-        var balance= 0;
-        var limit= 0;
+    switch (rowNumber) { //dummy values. Actual implementation to come later
+        case 1:
+            var name= "Discover IT Student";
+            var rewards= ["5% Cashback Restaurants", " 2% Cashback Grocery Stores", " 1% Cashback Everywhere"];
+            var benefits= "";
+            var balance= 407;
+            var limit= 1000;
+            break;
+        case 2:
+            var name= "Amazon Prime Rewards Card";
+            var rewards= ["5% Cashback Amazon", " 3% Cashback Gas Stations", " 4% Cashback Home Improvement", " 1% Cashback Everywhere"];
+            var benefits= "Interest-free Amazon payments";
+            var balance= 3970;
+            var limit= 4000;
+            break;
+        default:
+            var name= "Dummy Card";
+            var rewards= ["5% Cashback", " 3% Cashback", " 4% Cashback", " 1% Cashback"]
+            var benefits= "Useless";
+            var balance= 0;
+            var limit= 0;
     }
 
-    var delCard="<input type=button value=' X ' onClick='delRow()'>";
+    var delCard="<input type=button value=' X ' onclick='delRow()'>"; //last item is a button that deletes the card
 
-    var usage= balance + " / " + limit;
+    var usage= balance + " / " + limit; //usage is balance/limit. This is an easier way for the user to see
 
-    var rowdata = [name, rewards, benefits, usage, delCard];
+    var rowdata = [name, rewards, benefits, usage, delCard]; //setting up array which represents a row in the table
 
-    var tableRef = document.getElementById("cardTable");
+    var tableRef = document.getElementById("cardTable"); //table that holds cards
 
-    var newRow = tableRef.insertRow(tableRef.rows.length);
+    var newRow = tableRef.insertRow(tableRef.rows.length); //makes a new row
 
     newRow.onmouseover = function() {
         tableRef.clickedRowIndex = this.rowIndex;
@@ -40,24 +42,16 @@ function addRow()
     var i = 0;
     while (i < 5)
     {
-        newCell = newRow.insertCell(i);            // specify which column
-        newCell.innerHTML = rowdata[i];            // assign what content
+        newCell = newRow.insertCell(i);            // find column
+        newCell.innerHTML = rowdata[i];            // fill column
         newCell.onmouseover = this.rowIndex;       // attach row index to the row
         i++;
     }
+    rowNumber++;
 }
 
-function checkSearch() {
-    var msg = document.getElementById("usr-msg");
-    if (this.value.length == "a"){}
-    msg.textContent = "Search cannot be blank.";
-    alert("ayy");
-}
-var searchInput = document.getElementById("Search");
-searchInput.addEventListener('blur', checkSearch, false);
-
-delRow = () =>
+function delRow()
 {
-    if (confirm("Press OK to delete. This action is unrecoverable.") == true)
-        document.getElementById("cardTable").deleteRow(document.getElementById("cardTable").clickedRowIndex);
+    if (confirm("Are you sure you want to delete that card?") == true) //check if user really wants to delete card
+        document.getElementById("cardTable").deleteRow(document.getElementById("cardTable").clickedRowIndex); //remove row from table
 }
