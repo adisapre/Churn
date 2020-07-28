7d10359b7837fc006ffe8bc5bece4729b69bd66d
adisapre <adi.sapre@gmail.com>
2020-07-28 16:51:38 -0400

    added login and register functionality, cookies to come

--- php/login.php
+++ php/login.php
@@ -1,1 +1,22 @@
 <?php
+require("connect_db.php");
+
+$name = $_POST['username'];
+
+$password = md5($_POST['password']);
+//check for duplicate usernames
+$checker = "SELECT * FROM userinfo WHERE `username` = '$name' && `password` = '$password' ";
+$statement = $db->prepare($checker);
+$statement->execute();
+$result = $statement->fetchAll();
+$num = count($result);
+
+if ($num == 1){
+    echo "<script type='text/javascript'>window.location.href ='../index.html';</script>";
+
+}
+else{
+    echo "<script type='text/javascript'>alert('Username or Password is incorrect/not found');window.location.href ='../login.html';</script>";
+}
+
+?>
--- php/register.php
+++ php/register.php
@@ -1,1 +1,29 @@
 <?php
+require("connect_db.php");
+
+$name = $_POST['usernamereg'];
+
+$email = $_POST['email'];
+
+$password = md5($_POST['passwordreg']);
+//check for duplicate usernames
+$checker = "SELECT * FROM userinfo WHERE `username` = '$name'  ";
+$statement = $db->prepare($checker);
+$statement->execute();
+$result = $statement->fetchAll();
+$num = count($result);
+
+if ($num == 1){
+    echo "<script type='text/javascript'>alert('Username is taken, please try again');window.location.href ='../login.html';</script>";
+}
+else{
+    $query = "INSERT INTO userinfo (username, email, password) VALUES ('$name', '$email','$password')";
+    $statement = $db->prepare($query);
+    //run query
+    $statement->execute();
+    //release cursor
+    $statement->closeCursor();
+    header('location: ../index.html');
+}
+
+?>
