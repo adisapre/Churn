539bf66a2f48526d13d30a3f8dc92449c032e96b
Thomas Davis <tad8tt@virginia.edu>
2020-07-28 22:27:19 -0400

    adwada

--- php/cardAdder.php
+++ php/cardAdder.php
@@ -1,18 +1,33 @@
+<?php session_start();
+
+?>
 <?php
+if (isset($_SESSION['user']))
+{
+?>
 require("connect_db.php");
 
 $cardmodel = $_POST['cardModel'];
 
 $cardrewards = $_POST['cardRewards'];
-//check for duplicate usernames
+
+$cardbalance = $_GET['cardBalance'];
+
+$cardlimit = $_GET['cardLimit'];
+
+$username = $_SESSION['user'];
+//check for duplicate cards
 $checker = "SELECT * FROM cardtable WHERE `cardmodel` = '$cardmodel'";
 $statement = $db->prepare($checker);
 $statement->execute();
 $result = $statement->fetchAll();
 $num = count($result);
+echo $num;
+exit();
+
 
 if ($num == 1){
-    echo "<script type='text/javascript'>alert('Silly goose, you already have that card!');window.location.href ='../addCard.html';</script>";
+    echo "<script type='text/javascript'>alert('Silly goose, you already have that card!');window.location.href ='../addCard.php';</script>";
 
 }
 else{
@@ -26,6 +41,11 @@ else{
         $_SESSION['user'] = $name;
         $_SESSION['password'] = $password;
         header("location: ../index.php");
+}
+<?php
+}
+else{
+    header('Location: login.html');
 }
 
 ?>
